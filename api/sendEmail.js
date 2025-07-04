cd02210e576b558a288cb72479db4c29ebebdcc3
Morten Schou Gross <mgr@dst.dk>
2025-07-04 14:19:19 +0200

    return ok response

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -4,4 +4,5 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
     console.log('Hello world!');
+    return res.status(200).json({ message: `Hello World!` });
 }
